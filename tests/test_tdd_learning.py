a97bdc7bae67e781561bf144d8fbc349e2f05e81
Richard <richardjbrown1@gmail.com>
2019-01-21 15:30:35 -0700

    Step 2 adding three numbers

--- tests/test_tdd_learning.py
+++ tests/test_tdd_learning.py
@@ -9,22 +9,20 @@ Tests for `tdd_learning` module.
 """
 
 import pytest
+from tdd_learning.calc import Calc
 
 
-from tdd_learning import tdd_learning
+def test_add_two_numbers():
+    c = Calc()
+
+    res = c.add(4, 5)
+
+    assert res == 9
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+def test_add_three_numbers():
+    c = Calc()
 
+    res = c.add(4, 5, 6)
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument.
-    """
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+    assert res == 15
